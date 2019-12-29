d8e2807e7a28aed15073f835c0773f37fc70a465
Yue Yu <yuyue730@gmail.com>
2019-12-29 14:52:20 -0500

    Implement XML Http Request

---
+++ SourceCodes/src/types/index.ts
@@ -0,0 +1,14 @@
+export type Methods = 'get' | 'GET'
+  | 'delete' | 'DELETE'
+  | 'head' | 'HEAD'
+  | 'options' | 'OPTIONS'
+  | 'post' | 'POST'
+  | 'put' | 'PUT'
+  | 'patch' | 'PATCH';
+
+export interface AxiosRequestConfig {
+  url: string,
+  method?: Methods,
+  data?: any,
+  params?: any
+};
